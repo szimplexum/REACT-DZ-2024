5d2531b286b994fa0bf147754ffefbe5925ecf5c
szimplexum <daniel.zoltan@szimplexum.hu>
2024-09-12 16:54:54 +0200

    REACT Bootstap install

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   {
@@ -18,19 +18,10 @@ export default [
       },
     },
     settings: { react: { version: '18.3' } },
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-    },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -38,8 +29,10 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-];
+]
